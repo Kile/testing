587f287cf507087c23d1ea9bfd8fa12e2b78105f
Florian Crafter <55284712+FlorianStrobl@users.noreply.github.com>
2021-05-17 22:09:19 +0200

    CCs Formatting (and Types)
    Didn't checked everything, just overlooked it. I DIDN'T tested it!!!

--- functions.ts
+++ functions.ts
@@ -5,12 +5,12 @@ export function randomChoice(arr: Array<any>): any {
   return arr[~~(Math.random() * arr.length)];
 }
 
-export async function unknownCommand(message: discord.Message): Promise<any> {
-  await message.delete();
-  let msg = await message.reply(
-    'Unknown command ' + message.author.toMention()
+export async function unknownCommand(message: discord.Message): Promise<void> {
+  await message?.delete();
+  const msg = await message.reply(
+    `Unknown command ${ message.author.toMention() }`
   );
-  await setTimeout(() => msg.delete(), 5000);
+  await setTimeout(async () => await msg?.delete(), 5000);
 }
 
 export async function handleTest(message: discord.Message): Promise<any> {
@@ -30,9 +30,9 @@ export async function handleTest(message: discord.Message): Promise<any> {
     'test complete, further testing required, please re-test',
     'test complete'
   ];
-  //Advanced testing is defined here
+  // Advanced testing is defined here
   const isAdvanced = player.isAdvanced();
-  //The efficency does play a part of the items equipped
+  // The efficency does play a part of the items equipped
   const efficiencyStat = player.getStat('efficiency') + 1;
 
   // advanced tests have different dialog, and a fine chance of also failing.
@@ -74,9 +74,9 @@ export async function handleTest(message: discord.Message): Promise<any> {
     }
     // failed penalty, take away advanced testing permit.
     // obviously tests are more exciting if there is a risk to them!
-    if (choice.indexOf('failed') !== -1 && isAdvanced) {
+    if (choice.indexOf('failed') !== -1 && isAdvanced)
       await player.removeItem(2);
-    }
+
     return choice;
   });
 }
@@ -84,7 +84,8 @@ export async function handleTest(message: discord.Message): Promise<any> {
 export async function constructInventory(
   message: discord.Message
 ): Promise<discord.Embed> {
-  let u = await new User(+message.author.id).getData();
+  const u = await new User(+message.author.id).getData();
+
   let text;
   //@ts-ignore
   for (const [key, value] of Object.entries(u.inventory)) {
@@ -104,8 +105,8 @@ export async function constructInventory(
   }
   text = (text ?? 'No items\n') + '\n**Coins**\n' + u.coins.toString();
 
-  let embed = new discord.Embed({
-    title: 'Inventory for ' + message.author.getTag(),
+  const embed = new discord.Embed({
+    title: `Inventory for ${ message.author.getTag() }`,
     description: text
   });
   return embed;
@@ -114,15 +115,14 @@ export async function constructInventory(
 export async function buyItem(
   message: discord.Message,
   item: number
-): Promise<any> {
-  let u = await new User(+message.author.id).getData();
+): Promise<discord.Message> {
+  const u = await new User(+message.author.id).getData();
 
   // different response for items that exist, but are not buyable
-  if ([1, 2, 3, 6, 7, 8, 9].includes(item)) {
+  if ([1, 2, 3, 6, 7, 8, 9].includes(item))
     return await message.reply(
       'the shopkeeper looks at you and shakes his head solumnly.'
     );
-  }
 
   try {
     var price = (ITEMS as any)[item.toString()].cost;
@@ -130,15 +130,13 @@ export async function buyItem(
     return await message.reply('the shopkeeper looks at you curiously');
   }
 
-  if (u.hasItem(item)) {
+  if (u.hasItem(item))
     return await message.reply('the shopkeeper looks at you curiously');
-  }
 
-  if (u.coins < price) {
+  if (u.coins < price)
     return await message.reply(
       'the shopkeeper looks at you, then at the sign on the door, then motions you away dismissively.'
     );
-  }
 
   await u.removeCoins(price);
   await u.addItem(+item);
@@ -158,36 +156,38 @@ export async function sellItem(
   item: number,
   amount: number = 1
 ): Promise<discord.Message> {
-  let u = await new User(+message.author.id).getData();
-  if (!u.hasItem(item)) {
-    // If the item is not in possesion
+  const u = await new User(+message.author.id).getData();
+  
+  // If the item is not in possesion
+  if (!u.hasItem(item))
     return await message.reply(randomChoice(SELL_RESPONSES));
-  }
-  if (typeof (ITEMS as any)[item.toString()].price == 'string') {
-    // Not sellable item
+
+  // Not sellable item
+  if (typeof (ITEMS as any)[item.toString()].price == 'string')
     return await message.reply(randomChoice(SELL_RESPONSES));
-  }
-  if (amount > u.inventory[item.toString()]) {
-    // If the user tries to sell more than they have
+  
+  // If the user tries to sell more than they have
+  if (amount > u.inventory[item.toString()])
     return await message.reply(randomChoice(SELL_RESPONSES));
-  }
 
   await u.addCoins((ITEMS as any)[item.toString()].price * amount);
   await u.removeItem(item, amount);
+  
   return await message.reply('Success: Transaction Complete. Coins Deposited.');
 }
 
 export async function equipment(
   message: discord.Message
 ): Promise<discord.Embed> {
-  let u = await new User(+message.author.id).getData();
+  const u = await new User(+message.author.id).getData();
+  
   let embed = new discord.Embed({
-    title: message.author.getTag() + " 's equipped items"
+    title: `${ message.author.getTag() } 's equipped items`
   });
   for await (let s of SLOTS) {
     let count: number = 0;
     for (const [key, value] of Object.entries(u.inventory)) {
-      if ((ITEMS as any)[key.toString()].slot == s && value == true) {
+      if ((ITEMS as any)[key.toString()].slot == s && value === true) {
         embed.addField({
           name:
             s + ' (' + (ITEMS as any)[(key as any).toString()].quality + ')',
@@ -206,18 +206,16 @@ export async function equipment(
   }
   let stats;
   let skill;
-  for (const [key, value] of Object.entries(u.stats)) {
+  for (const [key, value] of Object.entries(u.stats))
     stats = (stats ?? '') + `**+${value}** ${key}`;
-  }
 
   embed.addField({
     name: 'Equipped Item Stats',
     value: stats ?? 'No stats yet'
   });
 
-  for (const [key, value] of Object.entries(u.skill)) {
+  for (const [key, value] of Object.entries(u.skill))
     skill = (skill ?? '') + `**${value}** ${key}`;
-  }
 
   embed.addField({ name: 'Skill Levels', value: skill ?? 'No skill yet' });
 
@@ -225,37 +223,38 @@ export async function equipment(
     name: 'Clearance Level',
     value: (await u.hasItem(3)) ? '1' : '0'
   });
-  //This needs to be changed in case more badges are added, I am just lazy for now
+  // This needs to be changed in case more badges are added, I am just lazy for now
   return embed;
 }
 
-export async function relax(message: discord.Message): Promise<any> {
-  let u = await new User(+message.author.id).getData();
-  if (u.hasItem(3)) {
-    //When author already has clearnce badge level 1
+export async function relax(message: discord.Message): Promise<discord.Message> {
+  const u = await new User(+message.author.id).getData();
+
+  // When author already has clearnce badge level 1
+  if (u.hasItem(3))
     return await message.reply(
       'you look around the break-room, and see your colleagues, you chat them up, but be careful not to mention any secrets beyond their clearance level'
-    );
-  }
-  if (u.hasItem(4) && u.hasItem(5)) {
-    // When author does not have the bdge but meets requirements
+
+  // When author does not have the bdge but meets requirements
+  if (u.hasItem(4) && u.hasItem(5))
     return await message.reply(
       'you enter the break room and sit down after a long day of testing. you sigh and throw your head back and kick your feet up on the table like you own the place. a man approaches you with a clipboard, looking closely at your helmet and shoes. "Take this." he mutters, sliding a badge to you across the table "You\'ve earned it, bud." he continues, before a loud horn blares again, and he shuffles away, leaving you there, glancing down at the badge in-front of you. the second horn sounds, snapping you out of your trance and you quickly snatch the card and slip it into your pocket, before heading back to work.'
     );
-  }
+
   await message.reply(
     'you look around the break-room, and see not a soul, a clock on the wall ticks. your internal voice reminds you to continue testing.'
   );
 }
 
 export function info(item: number): discord.Embed | string {
-  var data = (ITEMS as any)[item.toString()];
-  if (!data) {
-    return '**Error:** Database rejected query `SELECT * FROM items WHERE id = ? AND clearance_level <= 1` - please continue testing.';
-    // I am not adding the "redacted" thngs on purpose, once they serve a purpose they are worth adding
-  }
+  let data = (ITEMS as any)[item.toString()];
 
-  let embed = new discord.Embed({
+  if (data == null) {
+    // I am not adding the "redacted" thngs on purpose, once they serve a purpose they are worth adding
+    return '**Error:** Database rejected query `SELECT * FROM items WHERE id = ? AND clearance_level <= 1` - please continue testing.';
+  }    
+
+  const embed = new discord.Embed({
     title: '[' + data.name + ']',
     footer: { text: data.footer },
     fields: [
@@ -267,9 +266,9 @@ export function info(item: number): discord.Embed | string {
   });
   if (data.stats) {
     let stats;
-    for (const [key, value] of Object.entries(data.stats)) {
+    for (const [key, value] of Object.entries(data.stats))
       stats = (stats ?? '') + `**+${value}** ${key}\n`;
-    }
+
     embed.addField({ name: 'Stats', value: (stats ?? 'No stats').toString() });
   }
   embed.setColor((COLORS as any)[data.quality]);
@@ -280,19 +279,21 @@ export async function equip(
   message: discord.Message,
   item: number
 ): Promise<discord.Message> {
-  let u = await new User(+message.author.id).getData();
-  if (!u.hasItem(item)) {
+  const u = await new User(+message.author.id).getData();
+
+  if (!u.hasItem(item))
     return await message.reply(
       '**Error**: Equipment manager malfunction. Unknown item specified.'
     );
-  }
+
   const equipped: boolean = u.inventory[item];
-  if (typeof equipped !== 'boolean') {
+  if (typeof equipped !== 'boolean')
     return await message.reply(
       '**Error**: Equipment manager malfunction. Unknown item specified.'
     );
-  }
+
   await u.equip(item, equipped, message.member!);
+
   return await message.reply(
     `**Success**: Item **[${(ITEMS as any)[item.toString()].name}]** has been ${
       equipped ? '**unequipped** from' : '**equipped** to'
